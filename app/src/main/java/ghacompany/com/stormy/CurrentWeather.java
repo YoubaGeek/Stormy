a2ce3c0b03777d048c8bce8c5abb6747252cd13a
YoubaGeek <gharbiayoubesprit@gmail.com>
2015-04-07 18:49:23 +0100

    First commit

---
+++ app/src/main/java/ghacompany/com/stormy/CurrentWeather.java
@@ -0,0 +1,63 @@
+package ghacompany.com.stormy;
+
+/**
+ * Created by Ayoub on 4/6/2015.
+ */
+public class CurrentWeather {
+
+    private String mIcon;
+    private long mTime;
+    private double mTemperature;
+    private double mHumidity;
+    private double mPrecipChance;
+    private String mSummary;
+
+    public String getIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public double getTemperature() {
+        return mTemperature;
+    }
+
+    public void setTemperature(double temperature) {
+        mTemperature = temperature;
+    }
+
+    public double getHumidity() {
+        return mHumidity;
+    }
+
+    public void setHumidity(double humidity) {
+        mHumidity = humidity;
+    }
+
+    public double getPrecipChance() {
+        return mPrecipChance;
+    }
+
+    public void setPrecipChance(double precipChance) {
+        mPrecipChance = precipChance;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = summary;
+    }
+
+}
---
+++ app/src/main/java/ghacompany/com/stormy/MainActivity.java
@@ -0,0 +1,115 @@
+package ghacompany.com.stormy;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+/**
+ * Created by Ayoub on 4/6/2015.
+ */
+
+public class MainActivity extends ActionBarActivity {
+
+    public static  final  String TAG = MainActivity.class.getSimpleName();
+    private CurrentWeather mCurrentWeather;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String API_KEY = "cf62265b79b7f29f6ab95b4fbd0eb507";
+        double latitude = 37.8267;
+        double longitude = -122.423;
+        String forecastUrl = "https://api.forecast.io/forecast/"+API_KEY+"/"+latitude+","+longitude;
+
+        if(isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(forecastUrl)
+                    .build();
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Request request, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Response response) throws IOException {
+                    try {
+                        String jsonData = response.body().string();
+                        if (response.isSuccessful()) {
+                            Log.v(TAG, jsonData);
+                            mCurrentWeather = getCurrentDetails(jsonData);
+                        } else {
+                            alertUserAboutError();
+                        }
+                    }
+                    catch (IOException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                    catch (JSONException e) {
+                        Log.e(TAG, "Exception caught: ", e);
+                    }
+                }
+            });
+        }else{
+            Toast.makeText(this,"failed network",Toast.LENGTH_LONG).show();
+        }
+        Log.d(TAG, "Main UI Code");
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException{
+
+        //declare a jsonobject that contains the parent of the hierachy of the json data
+        JSONObject data = new JSONObject(jsonData);
+        //get timezone from the jsonobject and store it into a string variable
+        String timeZone = data.getString("timezone");
+        Log.i(TAG, timeZone);
+        //now declare another jsonobject that will contain the json data of a specific jsonobject of the json feed
+        JSONObject currently = data.getJSONObject("currently");
+        //declare an instance of the CurrentWeather class
+        CurrentWeather currentWeather = new CurrentWeather();
+        //set variables with json data parsed
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        Log.i(TAG, "HUMIDITY: " +currently.getDouble("humidity"));
+        currentWeather.setSummary(currently.getString("summary"));
+        Log.i(TAG, "SUMMARY: " +currently.getString("summary"));
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        Log.i(TAG, "PRECIPCHANCE: " +currently.getDouble("precipProbability"));
+        return currentWeather;
+    }
+
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+
+        return isAvailable;
+    }
+
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+        dialog.show(getFragmentManager(), "error_dialog");
+    }
+}
